eae0657412187fc190ba2e6abb0d67dbb850052e
Gerold55 <marleybeare56@gmail.com>
2020-04-07 21:05:48 -0400

    Adds Crafts

---
+++ crafts.lua
@@ -0,0 +1,17 @@
+minetest.register_craft({
+	output = 'edible_swords:swords_cc',
+	recipe = {
+	{'dye:pink','dye:pink','dye:pink',},
+	{'dye:pink','farming:wheat','dye:pink',},
+	{'default:paper','','',},
+	}
+})
+
+minetest.register_craft({
+	output = 'edible_swords:swords_dango',
+	recipe = {
+	{'dye:pink','dye:white','dye:yellow',},
+	{'','farming:wheat','',},
+	{'default:stick','','',},
+	}
+})
--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 edible_swords = {}
 
 dofile(minetest.get_modpath("edible_swords").."/swords.lua")
+dofile(minetest.get_modpath("edible_swords").."/crafts.lua")
